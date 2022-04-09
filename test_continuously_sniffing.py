82cab4fe2f0e2be7afa84b2973b884b6993c1fbf
cocoritz <67122306+cocoritz@users.noreply.github.com>
2022-04-09 11:34:03 +0100

    Update test_continuously_sniffing.py

--- test_continuously_sniffing.py
+++ test_continuously_sniffing.py
@@ -1,7 +1,32 @@
-def capture_live_packets(network_interface):
-    capture = pyshark.LiveCapture(interface=network_interface)
+#coline Ritz
+#Created on 07/03/2022
+#this file aims to sniff my network and send it to Mongodb 
+
+
+import pyshark 
+import pymongo
+import time
+from pymongo import MongoClient
+
+#URI (Uniform Resource Identifier) to connect to MongoDB database 
+client_URI = "mongodb+srv://coline:clondonc@cluster0.4olg7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+client = MongoClient(client_URI)
+db = client.Tshark # use or create a database named Twitter
+test_collection = db.test #use or create a collection named 
+test_collection.create_index([("id", pymongo.ASCENDING)],unique = True) # make sure the collected tweets are unique
+
+
+# define interface - how to capture all interface? 
+networkInterface = "en0"
+
+print("listening on %s" % networkInterface)
+
+
+def capture_live_packets(networkInterface):
+    # define capture object
+    capture = pyshark.LiveCapture(interface=networkInterface)
     for raw_packet in capture.sniff_continuously():
-        print(filter_all_tcp_traffic_file(raw_packet))
+        print(get_packet_details(raw_packet))
 
 def get_packet_details(packet):
     """
@@ -15,22 +40,27 @@ def get_packet_details(packet):
     destination_address = packet.ip.dst
     destination_port = packet[packet.transport_layer].dstport
     packet_time = packet.sniff_time
-    return f'Packet Timestamp: {packet_time}' \
-           f'\nProtocol type: {protocol}' \
-           f'\nSource address: {source_address}' \
-           f'\nSource port: {source_port}' \
-           f'\nDestination address: {destination_address}' \
-           f'\nDestination port: {destination_port}\n'
+    packet_length = packet.length
 
+    netpacket = { 
+                    'Packet Timestamp': packet_time, 
+                    'Protocol type': protocol,
+                    'Source address': source_address,
+                    'Source port': source_port,
+                    'Packet length': packet_length,
+                    'Destination address': destination_address,
+                    'Destination port': destination_port 
+                    }
 
-def filter_all_tcp_traffic_file(packet):
-    """
-    This function is designed to parse all the Transmission Control Protocol(TCP) packets
-    :param packet: raw packet
-    :return: specific packet details
-    """
-    if hasattr(packet, 'tcp'):
-       results = get_packet_details(packet)
-       return results
+    print(netpacket)
+    
+    return netpacket 
 
-capture_live_packets('en0')
+while True:
+
+    packet_capture = capture_live_packets('en0')
+    
+    # Store it!
+    net_id = test_collection.insert_one(packet_capture)
+    
+    
